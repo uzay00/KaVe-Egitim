a89e3786ff3004030b49187d6ecc483e24d73957
Uzay <uzay00@gmail.com>
2019-02-06 13:16:05 +0300

    yasamKocum uygulamasi eklendi

---
+++ VeriBilimi/play-video.py
@@ -0,0 +1,33 @@
+
+import time
+
+dk, sn = 0, 3
+sure = 60 * dk + sn # bekleme suresi
+time.sleep(sure) 
+
+
+import pygame
+pygame.init()
+
+pencere = pygame.display.set_mode((480,480)) # ((0, 0), pygame.FULLSCREEN) 
+pencere.fill(pygame.Color(250,0,0, 128))
+
+pygame.mixer.init()
+pygame.mixer.music.load('veri/genisys.mp3')
+pygame.mixer.music.play()
+
+
+
+devam_et = True
+while devam_et:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            devam_et = False
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_q:
+            devam_et = False
+    pygame.display.update()
+    pygame.time.Clock().tick(60)
+
+pygame.quit()
+quit()
