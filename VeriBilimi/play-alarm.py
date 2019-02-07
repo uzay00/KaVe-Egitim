910ae4342b664c5c8d5d11cd9efcc6cc65bad44a
Uzay <uzay00@gmail.com>
2019-02-07 19:43:02 +0300

    yasamkocum ve alarm kodu eklendi

--- VeriBilimi/play-alarm.py
+++ VeriBilimi/play-alarm.py
@@ -1,16 +1,20 @@
 
 import time
 
-dk, sn = 0, 3
+dk, sn = 10, 3
 sure = 60 * dk + sn # bekleme suresi
-time.sleep(sure) 
+time.sleep(sure - 2) 
 
 
 import pygame
 pygame.init()
 
-pencere = pygame.display.set_mode((480,480)) # ((0, 0), pygame.FULLSCREEN) 
+pencere = pygame.display.set_mode((275,175)) # ((0, 0), pygame.FULLSCREEN) 
 pencere.fill(pygame.Color(250,0,0, 128))
+
+resim = pygame.image.load('veri/terminator2.jpg')
+
+
 
 pygame.mixer.init()
 pygame.mixer.music.load('veri/genisys.mp3')
@@ -26,6 +30,7 @@ while devam_et:
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_q:
             devam_et = False
+    pencere.blit(resim,(0,0))
     pygame.display.update()
     pygame.time.Clock().tick(60)
 
